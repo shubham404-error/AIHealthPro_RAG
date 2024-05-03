db2ff60584d0d1bd84c2aa5be118965f38316ec0
Shubham Tejani <64226852+shubham404-error@users.noreply.github.com>
2024-05-04 02:32:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
 import streamlit as st
 from beyondllm import source, retrieve, embeddings, llms, generator
+from beyondllm.llms import GeminiModel
 from getpass import getpass
 from dotenv import load_dotenv
 load_dotenv()
 st.title("Chat with document")
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+google_api_key=os.getenv("GOOGLE_API_KEY")
 uploaded_file = st.file_uploader("Choose a PDF file", type='pdf')
 question = st.text_input("Enter your question")
 if uploaded_file is not None and question:
@@ -17,8 +18,9 @@ if uploaded_file is not None and question:
             f.write(uploaded_file.getbuffer())
 
         data = source.fit(file_path, dtype="pdf", chunk_size=1024, chunk_overlap=250)
+        llm = GeminiModel(model_name="gemini-pro",google_api_key)
         retriever = retrieve.auto_retriever(data,type="normal",top_k=3)
-        pipeline = generator.Generate(question=question, retriever=retriever)
+        pipeline = generator.Generate(question=question, retriever=retriever,llm=llm)
         response = pipeline.call()
         
         st.write(response)

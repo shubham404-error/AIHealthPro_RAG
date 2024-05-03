47ebb47cdf89f37a42e2f25a6d2d22477e52532b
Shubham Tejani <64226852+shubham404-error@users.noreply.github.com>
2024-05-04 02:25:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,29 +2,16 @@ import os
 import streamlit as st
 from beyondllm import source, retrieve, embeddings, llms, generator
 from getpass import getpass
-
-
+from dotenv import load_dotenv
+load_dotenv()
 st.title("Chat with document")
-
-st.text("Enter API Key")
-
-api_key = st.text_input("API Key:", type="password")
 os.environ['GOOGLE_API_KEY'] = api_key
-
-if api_key:
-    st.success("API Key entered successfully!")
-
-
-    uploaded_file = st.file_uploader("Choose a PDF file", type='pdf')
-
-
-    question = st.text_input("Enter your question")
-
-    if uploaded_file is not None and question:
-        
-        save_path = "./uploaded_files"
-        if not os.path.exists(save_path):
-            os.makedirs(save_path)
+uploaded_file = st.file_uploader("Choose a PDF file", type='pdf')
+question = st.text_input("Enter your question")
+if uploaded_file is not None and question:
+    save_path = "./uploaded_files"
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
         file_path = os.path.join(save_path, uploaded_file.name)
         with open(file_path, "wb") as f:
             f.write(uploaded_file.getbuffer())
